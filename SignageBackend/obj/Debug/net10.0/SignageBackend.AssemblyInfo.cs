1e74ae2e958b422f3278ced9e8c3646412cb2760
EduardoRD25 <manuel.eduardo.ramos.25@gmail.com>
2026-09-17 13:29:26 -0500

    feat:se agrega nueva imagen como prueba

--- SignageBackend/obj/Debug/net10.0/SignageBackend.AssemblyInfo.cs
+++ SignageBackend/obj/Debug/net10.0/SignageBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignageBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd7f0fe48804980b02d967b4634bd1ee205fb10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignageBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignageBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
